c70330c4358ded5c92b5a3ff1aac0eedf9a82e7c
Oscar Murrieta <oscarmlmurrieta@gmail.com>
2025-07-18 19:52:46 -0600

    refactor(main.rs): remove match pattern in main function

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,7 @@ mod shell;
 
 /// Entry point of the program
 /// Call the function that contains the shell main loop
-fn main() {
+fn main() -> Result<(), ShellError> {
     let mut shl = shell::Shl::new();
-
-    match shl.run() {
-        Ok(_) => println!("run method successfully run"),
-        Err(e) => match e {
-            ShellError::Parse(str) => println!("{}", str),
-            ShellError::IO(_) => eprintln!("Error I/O"),
-            ShellError::CommandNotFound(str) => eprintln!("{}", str),
-            ShellError::Exit => {}
-            ShellError::ArgNotFound(_) => {}
-            ShellError::DirNotFound(_) => {}
-            ShellError::HomeDirNotFound => {}
-        },
-    }
+    shl.run()
 }
